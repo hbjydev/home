63c0eb6624db94343bea99e5257a8cae09318069
Hayden Young <hi@hbjy.dev>
2022-11-14 03:12:12 +0000

    feat: making config work on mac

--- flake.nix
+++ flake.nix
@@ -2,29 +2,40 @@
   description = "Home Manager configuration of Hayden";
 
   inputs = {
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, ... }@inputs:
+  outputs = { self, home-manager, nixpkgs, neovim-nightly, darwin, ... }:
     let
-      system = "x86_64-linux";
+      configDarwin = import ./configuration/darwin;
+      configLinux = import ./configuration/linux;
       overlays = [
-        inputs.neovim-nightly.overlay
+        neovim-nightly.overlay
         (import ./overlays.nix)
       ];
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations.hayden = inputs.home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          { nixpkgs.overlays = overlays; }
-          ./home.nix
-        ];
+      darwinConfigurations = {
+        macbookair-personal =
+          configDarwin "hayden" { inherit darwin home-manager overlays; };
       };
+
+      # homeConfigurations.hayden = inputs.home-manager.lib.homeManagerConfiguration {
+      #   inherit pkgs;
+      #   modules = [
+      #     { nixpkgs.overlays = overlays; }
+      #     ./home-manager/home.nix
+      #   ];
+      # };
     };
 }
