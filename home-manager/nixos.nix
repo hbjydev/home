972b4e6123c4a48fae33041741740590d0707c97
Hayden Young <hayden@hbjy.dev>
2022-12-25 21:10:51 +0000

    feat: nixos-specific config tweaks

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -8,7 +8,37 @@ in
 
   home.file.".config/obs-studio/themes".source = (sources.catppuccin-obs + "/themes");
 
+  home.packages = with pkgs; [
+    discord
+    jetbrains.datagrip
+    postman
+    spotify
+    zotero
+
+    # fonts
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    open-sans
+    inter
+  ];
+
   gtk = import ./programs/gtk.nix { inherit pkgs; flk = self; };
+
+  dconf =
+    let
+      bgPath = "/home/hayden/.background";
+    in
+    {
+      settings = {
+        "org/gnome/desktop/background" = {
+          "picture-uri" = bgPath;
+        };
+        "org/gnome/desktop/screensaver" = {
+          "picture-uri" = bgPath;
+        };
+      };
+    };
+
+  fonts.fontconfig.enable = true;
 
   programs.chromium.enable = true;
 }
